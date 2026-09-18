e4f7f5c0de1a3a88bc006697962a17f98b1902c8
AlbertoTrevizo <betot.saenz@gmail.com>
2026-09-18 16:59:47 -0600

    header's typography changed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				mono: ['"Geist Mono Variable"', 'ui-monospace', 'monospace'],
+			},
+		},
 	},
 	plugins: [],
 }
